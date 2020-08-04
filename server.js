f633f6bbf97c050e182d564eb5a10d17d26ca64a
Glitch (hello-express) <none>
2020-08-04 13:39:03 +0000

    🍅⛹ Checkpoint ./server.js:20461970/1427

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
+//require('dotenv').config();
 
 app.get('/webhook', (req, res) => {
-    let VERIFY_TOKEN = process.env.
-    
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
+  let FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
 
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
 
-      // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === FB_VERIFY_TOKEN) {
 
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
 
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
-      }
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
     }
-    res.status(200).send(challenge);
+  }
+  else {
+    res.status(404).send('Missing information')
+  }
+});
+
+app.listen(process.env.PORT, function() {
+  console.log(`Server is running at port ${process.env.PORT}`);
 });
