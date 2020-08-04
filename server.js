34a775d621d0f62d8b48694a636087e6883b53e5
Glitch (hello-express) <none>
2020-08-04 13:51:09 +0000

    🚁🎑 Checkpoint ./server.js:20461970/413

--- server.js
+++ server.js
@@ -4,29 +4,25 @@ const app = express();
 
 app.get('/webhook', (req, res) => {
   let FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+  
   // Parse the query params
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
 
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-
     // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === FB_VERIFY_TOKEN) {
+  if (mode === 'subscribe' && 
+      token === FB_VERIFY_TOKEN
+     ) {
 
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
+    // Responds with the challenge token from the request
+    console.log('WEBHOOK_VERIFIED');
+    res.status(200).send(challenge);
 
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
-    }
-  }
-  else {
-    res.status(404).send('Missing information')
-  }
+  } else {
+    // Responds with '403 Forbidden' if verify tokens do not match
+    res.sendStatus(403);      
+}
 });
 
 app.listen(process.env.PORT, function() {
